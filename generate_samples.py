0ec18b28242297182b6366d638456e64e464cc45
Steve Howell <showell30@yahoo.com>
2024-12-31 07:29:56 +0000

    random.seed fails for me

--- generate_samples.py
+++ generate_samples.py
@@ -1,9 +1,5 @@
 import zerver.lib.event_schema_legacy
 
-import random
-
-random.seed(42)
-
 module_dict = zerver.lib.event_schema_legacy.__dict__
 print("SAMPLE_DATA = dict(")
 for k in sorted(module_dict):
