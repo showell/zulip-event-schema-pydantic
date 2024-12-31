d893bd4f34e02f9afc439de37230cdd9ba16ef74
Steve Howell <showell30@yahoo.com>
2024-12-31 12:29:08 +0000

    ruff

--- zerver/lib/event_schema.py
+++ zerver/lib/event_schema.py
@@ -8,12 +8,14 @@
 from zerver.lib.types import AnonymousSettingGroupDict
 from zerver.models import Realm, RealmUserDefault, Stream, UserProfile
 
+
 def make_checker(base_model):
     def f(name, event):
         base_model(event)
 
     return f
-    
+
+
 from pydantic_schema import (
     alert_words_event,
     attachment_add_event,
@@ -143,6 +145,7 @@ def check_delete_message(
 
     assert set(event.keys()) == keys
 
+
 def check_has_zoom_token(
     var_name: str,
     event: dict[str, object],
@@ -150,6 +153,7 @@ def check_has_zoom_token(
 ) -> None:
     _check_has_zoom_token(var_name, event)
     assert event["value"] == value
+
 
 def check_presence(
     var_name: str,
@@ -204,6 +208,7 @@ def check_realm_bot_update(
     assert isinstance(event["bot"], dict)
     assert {"user_id", field} == set(event["bot"].keys())
 
+
 def check_realm_emoji_update(var_name: str, event: dict[str, object]) -> None:
     """
     The way we send realm emojis is kinda clumsy--we
@@ -217,6 +222,7 @@ def check_realm_emoji_update(var_name: str, event: dict[str, object]) -> None:
     assert isinstance(event["realm_emoji"], dict)
     for k, v in event["realm_emoji"].items():
         assert v["id"] == k
+
 
 def check_realm_export(
     var_name: str,
@@ -239,6 +245,7 @@ def check_realm_export(
     assert has_export_url == (export["export_url"] is not None)
     assert has_deleted_timestamp == (export["deleted_timestamp"] is not None)
     assert has_failed_timestamp == (export["failed_timestamp"] is not None)
+
 
 def check_realm_update(
     var_name: str,
@@ -282,6 +289,7 @@ def check_realm_update(
     else:
         raise AssertionError(f"Unexpected property type {property_type}")
 
+
 def check_realm_default_update(
     var_name: str,
     event: dict[str, object],
@@ -295,6 +303,7 @@ def check_realm_default_update(
 
     prop_type = RealmUserDefault.property_types[prop]
     assert isinstance(event["value"], prop_type)
+
 
 def check_realm_update_dict(
     # handle union types
