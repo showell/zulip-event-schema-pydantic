298094309739e88aba64624e8db31a6039c53c41
Steve Howell <showell30@yahoo.com>
2025-01-02 00:14:42 +0000

    wip

--- generate_pydantic.py
+++ generate_pydantic.py
@@ -45,8 +45,6 @@ for k in module_dict:
         if not getattr(v, "__is_for_checker", False):
             v._name = fix_name(k)
 
-code = open("zerver/lib/event_schema.py").read()
-
 for k in sorted(module_dict):
     if k.endswith("_event") and not k.startswith("check_"):
         schema = module_dict[k]
@@ -55,7 +53,4 @@ for k in sorted(module_dict):
         k = k.replace("_event", "")
         k = k.replace('_', ' ').strip().title().replace(' ', '')
         k = "Event" + k
-        code = code.replace(orig_name, k)
         schema.print_full_pydantic(name=k)
-
-open("zerver/lib/event_schema.py", "w").write(code)
